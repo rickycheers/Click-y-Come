03638ca648f01798e704efd37821cdd1b6875098
Ricardo <ricardo.perez.porras@gmail.com>
2012-05-02 22:42:24 -0500

    Creating Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClickYCome::Application.config.secret_token = '92de273c0cd821a104d24b658e0c151a52b38bf493abc77875a6e8e564f802e5b8c5c74de06f7b837ba025a5d628f4200ef5a5ba8ab1c4986dd39384be6e89b2'
