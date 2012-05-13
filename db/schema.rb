3cd6701c10604d8d54e9a174d18aa08aa3ddf6c9
Ricardo <ricardo.perez.porras@gmail.com>
2012-05-13 12:48:13 -0500

    Modifying User model: Adding FB Access Token attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120508034020) do
+ActiveRecord::Schema.define(:version => 20120511024810) do
 
   create_table "dishes", :force => true do |t|
     t.string   "name"
@@ -70,8 +70,9 @@ ActiveRecord::Schema.define(:version => 20120508034020) do
     t.string   "email"
     t.integer  "phone"
     t.string   "office_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "fb_access_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
