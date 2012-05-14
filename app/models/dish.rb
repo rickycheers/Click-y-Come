d097b7296a853530832a9994f14f681111fe86ac
Ricardo <ricardo.perez.porras@gmail.com>
2012-05-13 19:47:20 -0500

    Updating controllers and views due to model change

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,5 @@
 class Dish < ActiveRecord::Base
-  attr_accessible :description, :image, :name, :type
+  attr_accessible :description, :image, :name, :dish_type
   has_many :menu_dishes
   has_many :menus, :through => :menu_dishes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120508034020) do
+ActiveRecord::Schema.define(:version => 20120514003429) do
 
   create_table "dishes", :force => true do |t|
     t.string   "name"
     t.string   "description"
     t.string   "image"
-    t.integer  "type"
+    t.integer  "dish_type"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
