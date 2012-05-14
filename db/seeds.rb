f75e753ff69e753befd341040ff560417051a5d5
Ricardo <ricardo.perez.porras@gmail.com>
2012-05-13 23:11:49 -0500

    Adding seed data for types table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+types = Type.create([{name: 'Plato Fuerte'}, {name: 'Acompanamiento'}, {name: 'Agua'}, {name: 'Postre'}])
